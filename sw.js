4dfc473595b7cc192ff8626f0f2acd9dfbac87b3
Jeremias Charts <jeremiascharts@users.noreply.github.com>
2026-09-09 05:49:12 -0300

    Always refresh the PWA entry page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ti68k-touch-v3';
+const CACHE_NAME = 'ti68k-touch-v4';
 const ASSETS = [
   './', './index.html', './manifest.webmanifest', './icon.svg', './icon-180.png',
   './v13_readable.js', './v12rom.js', './v12sav.js', './Ti-92plus.png',
@@ -9,6 +9,10 @@ self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then(response => response).catch(() => caches.match('./index.html')));
+    return;
+  }
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
     if (response.ok && new URL(event.request.url).origin === location.origin) caches.open(CACHE_NAME).then(cache => cache.put(event.request, response.clone()));
     return response;
